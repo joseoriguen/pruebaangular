2ef28549d9c62f1d28797ab2be3cc76c1f34cb79
jose <origuen253@hotmail.com>
2019-02-18 17:45:48 -0300

    primer commit

---
+++ src/app/clientes/clientes.json.ts
@@ -0,0 +1,10 @@
+import { Cliente } from "./cliente";
+
+export const CLIENTES: Cliente[] = [
+  {id: 1 , nombre:'Luis', apellido: 'Torres', createAt: '2017-11-11', email: 'luis@hotmail.com'},
+  {id: 2 , nombre:'Jose', apellido: 'Sanchez', createAt: '2017-11-12', email: 'jose@hotmail.com'},
+  {id: 3 , nombre:'Pedro', apellido: 'Pinto', createAt: '2017-11-13', email: 'pedro@hotmail.com'},
+  {id: 4 , nombre:'Maria', apellido: 'Mata', createAt: '2017-11-14', email: 'maria@hotmail.com'},
+  {id: 5 , nombre:'Andrea', apellido: 'Perez', createAt: '2017-11-15', email: 'andrea@hotmail.com'},
+  {id: 6 , nombre:'Juan', apellido: 'Mendez', createAt: '2017-11-16', email: 'juan@hotmail.com'}
+];
